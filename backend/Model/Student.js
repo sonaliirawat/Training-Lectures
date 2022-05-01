2773ba6ec8f78dff1eda95b7909b2aa0fcff24a5
Kramit02 <Amiteminem05@gmail.com>
2022-05-02 02:23:39 +0530

    new program

--- backend/Model/Student.js
+++ backend/Model/Student.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
+    name : { type : String},
+    age : { type : Number}
 
 })
 module.exports = mongoose.model('student', studentSchema);
--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,45 @@
 const express = require('express')
 const mongoDb = require('./mongoConnect')
+const bodyParser = require('body-parser')
+const studentModel = require('./Model/Student')
 const app = new express();
-app.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept,Authorization,Cache-Control',
-    );
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept,Authorization,Cache-Control',
+  );
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
 });
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.post('/addStudent', (req, res) => {
+  var model = req.body; //payload
+  studentModel.create(model, (error, result) => {
+    if (error) {
+      return res.json(false);
+    }
+    return res.json(true);
+  })
 
-app.get('/harrawala',(req,res)=>{
-     res.json(true);
 })
-app.get('/doiwala',(req,res)=>{
-    res.json();
+app.get('/studentList', (req, res) => {
+  studentModel.find((error, result) => {
+    if (error) {
+      return res.json(false);
+    }
+    return res.json(result);
+  })
+  
 })
 
-app.listen(4000,function(){
-    console.log('test')
+app.listen(4000, function () {
+  console.log('test')
 });
